9e95ab9c14a5f7c55dc4b67e6238c5f57845ff50
Ryan Joyner <joynerryan88@gmail.com>
2018-11-16 14:47:27 -0800

    initial folder setup and component organization

---
+++ src/components/landing/index.js
@@ -0,0 +1,14 @@
+import React, { Component, Fragment } from "react";
+import SignIn from "./SignIn";
+
+class Landing extends Component {
+  render() {
+    return (
+      <Fragment>
+        <SignIn />
+      </Fragment>
+    );
+  }
+}
+
+export default Landing;
